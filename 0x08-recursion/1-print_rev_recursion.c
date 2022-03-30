d0b41bd48cf08f2667a87fdbffc4fe26144fdb64
Trizah Mukung <mukungtrizah@gmail.com>
2022-03-30 19:11:11 +0000

    Prints string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse 
- * @s: Pointer to string to be reversed 
+ * _print_rev_recursion - Prints a string in reverse. 
+ * @s: Pointer to string to be reversed
  */
 void _print_rev_recursion(char *s)
 {
@@ -11,4 +11,4 @@ void _print_rev_recursion(char *s)
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-}	
+}
