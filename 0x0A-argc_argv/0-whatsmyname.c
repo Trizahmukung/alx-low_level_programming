97ad23980910662988e9fff78ba00f19baac87f2
Trizah Mukung <mukungtrizah@gmail.com>
2022-04-06 14:15:20 +0000

    Print name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,8 +3,8 @@
 /**
  * main - Prints name of program.
  *
- * @argc: Number of arguments passed.
- * @argv: Array of pointers to be passed.
+ * @argc: Number of arguments passed on the CLI.
+ * @argv: Array of pointers to string arguments passed.
  *
  * Return: Always 0.
  */
@@ -12,5 +12,6 @@ int main(int argc, char *argv[])
 {
 	if (argc > 0)
 		printf("%s\n", argv[0]);
-	return(0);
+
+	return (0);
 }
